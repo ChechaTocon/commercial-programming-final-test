2f9eafc5fa95aae94d4a499253440caa98f11e6a
Celeste Galicia <celes0_33@hotmail.es>
2020-12-12 12:32:55 -0600

    conexion a la base de datos MYSQL

--- backend/movies/settings.py
+++ backend/movies/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'FinalPrograDB',
+        'USER': 'root',
+        'PASSWORD': 'bow7NDyFbyFvG0ju',
+        'HOST':'34.75.119.133',
+        'PORT':'3306'
     }
 }
 
