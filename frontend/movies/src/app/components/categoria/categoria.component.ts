d7503fedbd571549aaf6385ed3054cc452db08cd
anthonyegs <anthonyegs@gmail.com>
2020-12-12 22:02:35 -0600

    Query al endpoint desde el frontend
    -Se hizo el query de las categorias con apollo
    -Se agrego el cors en el backend
    -Se modifico el endpoint en el archivo de configuracion de graphql.module.ts

--- frontend/movies/src/app/components/categoria/categoria.component.ts
+++ frontend/movies/src/app/components/categoria/categoria.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { Subscription } from 'rxjs'
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Category } from '../../models/category.model'
+
+const categoryQuery = gql`
+  query {
+    category {
+      id
+      name
+      color
+      createdAt
+    }
+  }
+`;
 
 @Component({
   selector: 'app-categoria',
@@ -6,10 +22,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria.component.css']
 })
 export class CategoriaComponent implements OnInit {
+  dataSource: Category[] | undefined
+  private querySubscription: Subscription | undefined;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: categoryQuery
+    })
+      .valueChanges
+      .subscribe(({ data }) => {
+        this.dataSource = data.category;
+        console.log(this.dataSource)
+      });
   }
 
 }
