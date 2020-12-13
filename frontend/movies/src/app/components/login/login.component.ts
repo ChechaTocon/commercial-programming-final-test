347c37bdd230a6561cf50443a36acc12cee45e6e
ChechaTocon <49048007+ChechaTocon@users.noreply.github.com>
2020-12-12 18:38:11 -0600

    icon arregaldo

--- frontend/movies/src/app/components/login/login.component.ts
+++ frontend/movies/src/app/components/login/login.component.ts
@@ -3,9 +3,23 @@ import { Component, OnInit, DoCheck } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { from } from 'rxjs';
+import { Subscription } from 'rxjs'
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 import { User } from 'src/app/models/user.model';
 import { UserService} from '../../services/user.service'
 
+
+const userLoginQuery = gql`
+  query getJuegos {
+  juegos {
+    id
+    nombre
+    descripcion
+    foto
+  }
+}
+`;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
